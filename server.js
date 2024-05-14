1a781f48ae7b51a8a49601a280713a783ca4b1ec
NotAtitYet <kumar.sonu.ece21@itbhu.ac.in>
2024-05-14 12:46:51 +0530

    proxy setup

---
+++ server.js
@@ -0,0 +1,62 @@
+import express from 'express';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+import fetch from 'node-fetch';
+const app = express();
+app.use(express.json());
+app.use(cors());
+const PORT = process.env.PORT || 8000;
+
+// Middleware to parse JSON bodies
+app.use(bodyParser.json());
+
+// Define a route to forward requests to the API server
+app.all('/api/*', async (req, res) => {
+  try {
+    console.log(req.body);
+    // console.log(req.headers);
+    const headers = req.url == '/api/ValidateLogin' ? {
+      "Content-Type": "application/json",
+      "AppName": req.headers.appname,
+      "SecretKey": req.headers.secretkey
+    } : {
+      "Content-Type": "application/json",
+      "Authorization": "Basic " + req.headers.authorization
+    };
+    const url = `https://demoapi.emsigner.com${req.url}`; // Replace 'API_BASE_URL' with the base URL of the API server
+    // console.log(headers);
+    if (req.method === "POST") {
+      const response = await fetch(url, {
+        method: req.method,
+        headers:headers,
+        body: JSON.stringify(req.body),
+      })
+        .then(response => response.json())
+        .then(data => {
+          console.log(data);
+          res.json(data);
+        })
+    } else {
+      const response = await fetch(url, {
+        method: req.method,
+        headers: headers,
+        // body: JSON.stringify(req.body),
+      })
+        .then(response => response.json())
+        .then(data => {
+          console.log(data);
+          res.json(data);
+        })
+    }
+
+
+  } catch (error) {
+    console.error('Proxy server error:', error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Proxy server listening at http://localhost:${PORT}`);
+});
